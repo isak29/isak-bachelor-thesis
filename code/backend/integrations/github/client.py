0d8d269479542e375bcf730f7b92e4239a0402ed
isak29 <lampell.i@gmail.com>
2026-03-27 15:25:50 +0100

    Api token for Github, and first integration test completet.

--- code/backend/integrations/github/client.py
+++ code/backend/integrations/github/client.py
@@ -1,3 +1,23 @@
-
-
 # Ansvarar för autentisering och API anrop
+
+import requests
+
+token = "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"
+
+headers = {"Authorization": f"Bearer {token}"
+
+}
+
+url = "https://api.github.com/user"
+
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data["login"])
+    print(data["public_repos"])
+else:
+    print("Det uppstod ett fel:", response.status_code)
+
+
